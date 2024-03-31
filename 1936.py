9dd13fd276f7f66b90594ac2ad80854d9d08304d
clov2r <y0uaremysumm2r@gmail.com>
2024-03-31 18:42:08 +0900

    D1
    D1

---
+++ 1936.py
@@ -0,0 +1,17 @@
+# SWEA D1
+A, B=map(int, input().split())
+if (A==1):
+    if(B==2):
+        print("A")
+    elif(B==3):
+        print("B")
+if (A==2):
+    if(B==1):
+        print("A")
+    elif(B==3):
+        print("B")
+if (A==3):
+    if(B==1):
+        print("B")
+    elif(B==2):
+        print("A")
